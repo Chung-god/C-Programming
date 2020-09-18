831a62040b91c62109030cb37a1d81c020bd4c9c
HyunChung Park <54928780+Chung-god@users.noreply.github.com>
2020-09-18 12:06:28 +0900

    ch1 , ch2

---
+++ ch1/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+'#' - 전처리기
+'include' - 뒤에 오는 파일을 소스에 포함하라는 C언어의 예약어
+'stdio.h' - 'Standard Input/Output'을 의미하는 표준입출력 헤더  
+*/
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
